806f43707e65b021332565b08103c8a49ee7ea66
Eriiin <erinnliao@gmail.com>
2024-03-14 04:22:43 +0800

    build: add i18n plugin

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import i18n from './i18n'
 
 loadFonts()
 
-createApp(App)
+createApp(App).use(i18n)
   .use(router)
   .use(vuetify)
   .mount('#app')
